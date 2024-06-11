59e92431c46303a3533db4cee150e5f2a51fe8f3
Max Kim <50355670+holymollyhao@users.noreply.github.com>
2024-06-11 15:13:53 +0900

    cleanup

--- src/run_exp.sh
+++ src/run_exp.sh
@@ -1,2 +1,1 @@
-python train.py --batch_size 64 --data_parallel --lr 2e-5 --wandb --output_dir clip_adaptive_tune_wzoom_textencoder --cumulative_grad_steps 4 --eval_step 100 --flip_handler zoom --flips "adaptive_flip"
-python train.py --batch_size 64 --data_parallel --lr 2e-5 --wandb --output_dir clip_visual_prompt_textencoder --cumulative_grad_steps 4 --eval_step 100 --flip_handler none --flips "adaptive_flip"
+python train.py --batch_size 64 --data_parallel --lr 2e-5 --wandb --output_dir clip_raaug --cumulative_grad_steps 4 --eval_step 100 --flip_handler zoom --flips "relation_aware_flip"
